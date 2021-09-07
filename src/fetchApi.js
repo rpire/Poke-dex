357b42960b53533d64d9525adffeb25aefb619f6
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-09-07 18:15:14 +0100

    Add New Function To Fetch Each Pokemon

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,17 +1,21 @@
 // eslint-disable-next-line import/no-cycle
 import displayPokemon from './displayPokes.js';
 
+const fetchEachPoke = (pokemon) => {
+  const pokemonUrl = pokemon.url;
+  fetch(pokemonUrl)
+    .then((response) => response.json())
+    .then((pokemonData) => {
+      displayPokemon(pokemonData);
+    });
+};
+
 const fetchApi = () => {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0')
     .then((response) => response.json())
     .then((Allpokes) => {
       Allpokes.results.forEach((poke) => {
-        const pokemonUrl = poke.url;
-        fetch(pokemonUrl)
-          .then((response) => response.json())
-          .then((pokemonData) => {
-            displayPokemon(pokemonData);
-          });
+        fetchEachPoke(poke);
       });
     });
 };
