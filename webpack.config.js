b595024917729e247d7a76d26a88138574aed45a
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-09-07 08:18:16 +0100

    Added Assets Folder To Dist Directory

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
       },
     ],
   },
