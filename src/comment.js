ccc0e8d397cd3a95b62e0dd908a812790c0e12ed
rpire <rubenpire7@gmail.com>
2021-09-08 18:20:02 -0400

    Run npm build

--- src/comment.js
+++
@@ -1,6 +0,0 @@
-const involvementURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ewWKj7zRfM05pYQRcA4r/comments';
-
-const getComments = (itemID) => fetch(`${involvementURL}?item_id=${itemID}`)
-  .then((response) => response.json());
-
-export default getComments;
