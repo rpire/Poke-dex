c88d278c6984b6487630ce670f431d8d2e45e1ce
rpire <rubenpire7@gmail.com>
2021-09-08 00:53:04 -0400

    Make slight changes to displayPokes and fetchApi to make them work better with event listeners

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,23 +1,10 @@
 // eslint-disable-next-line import/no-cycle
 import displayPokemon from './displayPokes.js';
 
-const fetchEachPoke = (pokemon) => {
-  const pokemonUrl = pokemon.url;
-  fetch(pokemonUrl)
-    .then((response) => response.json())
-    .then((pokemonData) => {
-      displayPokemon(pokemonData);
-    });
-};
-
 const fetchApi = () => {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0')
     .then((response) => response.json())
-    .then((Allpokes) => {
-      Allpokes.results.forEach((poke) => {
-        fetchEachPoke(poke);
-      });
-    });
+    .then((Allpokes) => displayPokemon(Allpokes.results));
 };
 
 export default fetchApi;
