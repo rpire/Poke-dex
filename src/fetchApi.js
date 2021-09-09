1a9bda84a700473cbb42923ff0df9aaa76e22bf0
rpire <rubenpire7@gmail.com>
2021-09-09 06:05:47 -0400

    Change the pokemon displaying to 18

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,5 +1,5 @@
 const fetchApi = async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0');
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=18&offset=0');
   return response.json();
 };
 
